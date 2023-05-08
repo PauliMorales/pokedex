91b887ac97cf23f19f0583e4b79f25a086282eec
PauliMorales <paumor271@gmail.com>
2023-05-07 21:01:38 -0500

    resolve error in pokecard path

--- src/components/Pokedex/PokeCard.jsx
+++ src/components/Pokedex/PokeCard.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import useFetch from "../../hooks/UseFetch";
-import "./styles/pokeCard.css";
+import "./styles/pokecard.css";
 import { useNavigate } from "react-router-dom";
 
 const PokeCard = ({ url }) => {
